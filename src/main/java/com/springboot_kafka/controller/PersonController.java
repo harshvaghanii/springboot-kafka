f6fbcea75cb80d8415c9209e559944f14f85fbf6
Harsh Vaghani <75880484+harshvaghanii@users.noreply.github.com>
2025-02-01 11:49:27 -0600

    Adding JSON Deserialization in Kafka (#1)
    Adding a JSON Type Kafka Producer and changing the properties to accept JSON type data

---
+++ src/main/java/com/springboot_kafka/controller/PersonController.java
@@ -0,0 +1,39 @@
+package com.springboot_kafka.controller;
+
+import com.springboot_kafka.dummyobjects.Person;
+import com.springboot_kafka.kafka.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/person")
+public class PersonController {
+
+    private final KafkaProducer kafkaProducer;
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("This works!");
+    }
+
+    @GetMapping(path = "/send")
+    public ResponseEntity<Person> sendPerson() {
+        // Creates a dummy Person and sends it to Kafka Queue
+        Random random = new Random();
+        Person person = Person.builder()
+                .name(STR."User-\{UUID.randomUUID().toString().substring(0, 5)}") // Generates a random name
+                .age(random.nextInt(100)) // Random age between 0-99
+                .email(STR."user\{random.nextInt(1000)}@example.com") // Random email
+                .build();
+        kafkaProducer.sendPerson(person);
+        return ResponseEntity.ok(person);
+    }
+
+}
--- src/main/java/com/springboot_kafka/kafka/KafkaProducer.java
+++ src/main/java/com/springboot_kafka/kafka/KafkaProducer.java
@@ -1,8 +1,13 @@
 package com.springboot_kafka.kafka;
 
+import com.springboot_kafka.dummyobjects.Person;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +16,26 @@ import org.springframework.stereotype.Service;
 public class KafkaProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Person> personKafkaTemplate;
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    @Value("${spring.kafka.topic-json.name}")
+    private String jsonTopicName;
 
     public void sendMessage(String message) {
         log.info("Sent message: {}", message);
-        kafkaTemplate.send("javaguides", message);
+        kafkaTemplate.send(topicName, message);
+    }
+
+    public void sendPerson(Person person) {
+        Message<Person> message = MessageBuilder
+                .withPayload(person)
+                .setHeader(KafkaHeaders.TOPIC, jsonTopicName)
+                .build();
+        log.info("This is the person in Message format: {}", message);
+        kafkaTemplate.send(message);
     }
 
 }
