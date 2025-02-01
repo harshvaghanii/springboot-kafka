fa0edf127144dd0e6dc9f58fa4cee8b771827baf
harshvaghanii <harshvaghani98@gmail.com>
2025-02-01 11:45:45 -0600

    externalizing the group id

--- src/main/java/com/springboot_kafka/kafka/KafkaConsumer.java
+++ src/main/java/com/springboot_kafka/kafka/KafkaConsumer.java
@@ -2,6 +2,7 @@ package com.springboot_kafka.kafka;
 
 import com.springboot_kafka.dummyobjects.Person;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "groupOne")
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.groupId}")
     public void consume(String message) {
         log.info("This is the message: {}", message);
     }
 
-    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "groupOne")
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.groupId}")
     public void consumePerson(Person person) {
         log.info("This is the person sent by Kafka: {} ", person);
     }
